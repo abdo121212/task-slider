e1009f2224b187f21df946741d1e50ac1984a8e7
Abdallah Atef <atfbdallh147@gmail.com>
2025-02-05 17:57:29 +0200

    update slider

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-let bookCart = Array.from(document.querySelectorAll(".book-card"));
+document.addEventListener("DOMContentLoaded", function () {
+  let bookCart = Array.from(document.querySelectorAll(".book-card"));
+  const displayedImage = document.getElementById("displayed-image");
 
-const displayedImage = document.getElementById("displayed-image");
+  bookCart.forEach((box) => {
+    box.addEventListener("click", function () {
+      bookCart.forEach((ele) => {
+        ele.classList.remove("active");
+        this.classList.add("active");
+      });
 
-bookCart.forEach((box) => {
+      const imgSrc = this.getAttribute("data-img");
 
-  box.addEventListener("click", function () {
-    bookCart.forEach((ele)=>{
-      ele.classList.remove("active")
-      this.classList.add("active")
-    })
-    const imgSrc = this.getAttribute("data-img");
-    displayedImage.src = imgSrc;
+      displayedImage.src = imgSrc;
+    });
+  });
+
+  bookCart.forEach((card) => {
+    card.addEventListener("click", function () {
+      const newImgSrc = this.getAttribute("data-img");
+
+      displayedImage.classList.add("rotate-animation");
+
+      setTimeout(() => {
+        displayedImage.src = newImgSrc;
+      }, 250);
+
+      setTimeout(() => {
+        displayedImage.classList.remove("rotate-animation");
+      }, 500);
+    });
   });
 });
